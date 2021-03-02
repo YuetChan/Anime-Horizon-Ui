2d5398ec081b1d4c777cf815e40c2cc21066a269
cheuk.chan <cheuk.chan@accesscfa.com>
2021-03-02 16:46:32 -0500

    update primng version to  11

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/Pitbull-Investors-Ui'),
+      dir: require('path').join(__dirname, './coverage/Anime-Horizon-Ui'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
